8cc45f640502bdb3824ef386255ba35a9b52b168
sjtrny <sjtrny@gmail.com>
2024-10-27 14:09:54 +1100

    Add probability estimates

--- src/app.py
+++ src/app.py
@@ -3,10 +3,11 @@ import numpy as np
 import plotly.graph_objects as go
 from dash import ALL, Dash, Input, Output, callback, ctx, dcc, html
 from dash.exceptions import PreventUpdate
+from scipy.integrate import nquad
 from scipy.stats import beta, uniform
 
-num_points_restricted = 50000
-num_points_full = 20000
+num_points_mc = 1000000
+num_points_restricted = 20000
 
 distribution_defaults = {
     "Uniform": {"class": uniform, "parameters": {}},
@@ -31,15 +32,76 @@ app.layout = dbc.Container(
                     [
                         dbc.Card(
                             [
-                                dbc.CardHeader([
-                                    "Joint Density",
-                                    dcc.Checklist(
-                                        options=["Restrict to Integration Region"],
-                                        value=["Restrict to Integration Region"],
-                                        inline=True,
-                                        id="integration-region",
-                                    )
-                                ], className='d-flex justify-content-between'),
+                                dbc.CardHeader(
+                                    [
+                                        "Probability Estimates",
+                                    ]
+                                ),
+                                dbc.CardBody(
+                                    [
+                                        dbc.ListGroup(
+                                            [
+                                                dbc.ListGroupItem(
+                                                    [
+                                                        html.Div(
+                                                            [
+                                                                html.H5(
+                                                                    "Integral",
+                                                                    className="mb-1",
+                                                                ),
+                                                            ],
+                                                            className="d-flex w-100 justify-content-between",
+                                                        ),
+                                                        html.P(
+                                                            className="mb-1",
+                                                            id="estimate-integral",
+                                                        ),
+                                                    ]
+                                                ),
+                                                dbc.ListGroupItem(
+                                                    [
+                                                        html.Div(
+                                                            [
+                                                                html.H5(
+                                                                    "Monte Carlo",
+                                                                    className="mb-1 mt-2",
+                                                                ),
+                                                            ],
+                                                            className="d-flex w-100 justify-content-between",
+                                                        ),
+                                                        html.P(
+                                                            className="mb-1",
+                                                            id="estimate-montecarlo",
+                                                        ),
+                                                    ]
+                                                ),
+                                            ],
+                                            flush=True,
+                                        )
+                                    ]
+                                ),
+                            ]
+                        )
+                    ],
+                    lg=2,
+                    className="mt-3",
+                ),
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardHeader(
+                                    [
+                                        "Joint Density",
+                                        dcc.Checklist(
+                                            options=["Restrict to Integration Region"],
+                                            value=["Restrict to Integration Region"],
+                                            inline=True,
+                                            id="integration-region",
+                                        ),
+                                    ],
+                                    className="d-flex justify-content-between",
+                                ),
                                 dbc.CardBody(
                                     [
                                         dcc.Graph(
@@ -51,7 +113,8 @@ app.layout = dbc.Container(
                             ]
                         )
                     ],
-                    lg=6, className="mt-3",
+                    lg=5,
+                    className="mt-3",
                 ),
                 dbc.Col(
                     [
@@ -69,12 +132,12 @@ app.layout = dbc.Container(
                             ]
                         )
                     ],
-                    lg=6, className="mt-3",
+                    lg=5,
+                    className="mt-3",
                 ),
             ],
-
         ),
-        dbc.Row(html.Hr(className='mt-3 mb-0')),
+        dbc.Row(html.Hr(className="mt-3 mb-0")),
         dbc.Row(
             [
                 dbc.Col(
@@ -125,7 +188,8 @@ app.layout = dbc.Container(
                             ),
                         ]
                     ),
-                    lg=4, className="mt-3",
+                    lg=4,
+                    className="mt-3",
                 ),
                 dbc.Col(
                     dbc.Card(
@@ -175,7 +239,8 @@ app.layout = dbc.Container(
                             ),
                         ]
                     ),
-                    lg=4, className="mt-3",
+                    lg=4,
+                    className="mt-3",
                 ),
                 dbc.Col(
                     dbc.Card(
@@ -225,7 +290,8 @@ app.layout = dbc.Container(
                             ),
                         ]
                     ),
-                    lg=4, className="mt-3",
+                    lg=4,
+                    className="mt-3",
                 ),
             ]
         ),
@@ -299,6 +365,8 @@ def update_distribution(distribution3_dropdown):
 @callback(
     Output("joint-plot", "figure"),
     Output("marginal-plot", "figure"),
+    Output("estimate-integral", "children"),
+    Output("estimate-montecarlo", "children"),
     inputs=dict(
         integration_region=Input("integration-region", "value"),
         dist1=dict(
@@ -324,58 +392,74 @@ def update_graph(integration_region, dist1, dist2, dist3):
         and len(ctx.args_grouping.dist1["params"]) == 0
     ):
         raise PreventUpdate
-    x_dist = distribution_defaults[dist1["dropdown"]]["class"](
-        loc=dist1["range"][0] / 10,
-        scale=dist1["range"][1] / 10 - dist1["range"][0] / 10,
-        **{
-            param["id"]["index"]: param["value"]
-            for param in ctx.args_grouping.dist1["params"]
-        },
-    )
-
 
     if (
         ctx.args_grouping.dist2["dropdown"]["triggered"] == True
         and len(ctx.args_grouping.dist2["params"]) == 0
     ):
         raise PreventUpdate
-    y_dist = distribution_defaults[dist2["dropdown"]]["class"](
-        loc=dist2["range"][0] / 10,
-        scale=dist2["range"][1] / 10 - dist2["range"][0] / 10,
-        **{
-            param["id"]["index"]: param["value"]
-            for param in ctx.args_grouping.dist2["params"]
-        },
-    )
-
 
     if (
         ctx.args_grouping.dist3["dropdown"]["triggered"] == True
         and len(ctx.args_grouping.dist3["params"]) == 0
     ):
         raise PreventUpdate
+
+    x_dist_range = (dist1["range"][0] / 10, dist1["range"][1] / 10)
+    y_dist_range = (dist2["range"][0] / 10, dist2["range"][1] / 10)
+    z_dist_range = (dist3["range"][0] / 10, dist3["range"][1] / 10)
+
+    x_dist = distribution_defaults[dist1["dropdown"]]["class"](
+        loc=x_dist_range[0],
+        scale=x_dist_range[1] - x_dist_range[0],
+        **{
+            param["id"]["index"]: param["value"]
+            for param in ctx.args_grouping.dist1["params"]
+        },
+    )
+    y_dist = distribution_defaults[dist2["dropdown"]]["class"](
+        loc=y_dist_range[0],
+        scale=y_dist_range[1] - y_dist_range[0],
+        **{
+            param["id"]["index"]: param["value"]
+            for param in ctx.args_grouping.dist2["params"]
+        },
+    )
     z_dist = distribution_defaults[dist3["dropdown"]]["class"](
-        loc=dist3["range"][0] / 10,
-        scale=dist3["range"][1] / 10 - dist3["range"][0] / 10,
+        loc=z_dist_range[0],
+        scale=z_dist_range[1] - z_dist_range[0],
         **{
             param["id"]["index"]: param["value"]
             for param in ctx.args_grouping.dist3["params"]
         },
     )
 
-    if integration_region:
-        x = x_dist.rvs(size=num_points_restricted)
-        y = y_dist.rvs(size=num_points_restricted)
-        z = z_dist.rvs(size=num_points_restricted)
-        condition = ((x > y) & (x < z)) | ((x < y) & (x > z))
+    x_sample = x_dist.rvs(size=num_points_mc)
+    y_sample = y_dist.rvs(size=num_points_mc)
+    z_sample = z_dist.rvs(size=num_points_mc)
 
-        x_region = x[condition]
-        y_region = y[condition]
-        z_region = z[condition]
+    condition = ((x_sample > y_sample) & (x_sample < z_sample)) | (
+        (x_sample < y_sample) & (x_sample > z_sample)
+    )
+
+    mc_estimate = np.sum(condition) / num_points_mc
+
+    if integration_region:
+        x_region = x_sample[condition]
+        y_region = y_sample[condition]
+        z_region = z_sample[condition]
     else:
-        x_region = x_dist.rvs(size=num_points_full)
-        y_region = y_dist.rvs(size=num_points_full)
-        z_region = z_dist.rvs(size=num_points_full)
+        x_region = x_sample
+        y_region = y_sample
+        z_region = z_sample
+
+    region_idxs = np.random.choice(
+        np.arange(x_region.size), size=num_points_restricted, replace=False
+    )
+
+    x_region = x_region[region_idxs]
+    y_region = y_region[region_idxs]
+    z_region = z_region[region_idxs]
 
     joint_plot = go.Figure(
         data=[
@@ -404,32 +488,26 @@ def update_graph(integration_region, dist1, dist2, dist3):
         height=400,
     )
 
-    x_range = np.linspace(dist1["range"][0] / 10, dist1["range"][1] / 10, num=100, endpoint=True)
-    y_range = np.linspace(dist2["range"][0] / 10, dist2["range"][1] / 10, num=100, endpoint=True)
-    z_range = np.linspace(dist3["range"][0] / 10, dist3["range"][1] / 10, num=100, endpoint=True)
+    x_range = np.linspace(x_dist_range[0], x_dist_range[1], num=100, endpoint=True)
+    y_range = np.linspace(y_dist_range[0], y_dist_range[1], num=100, endpoint=True)
+    z_range = np.linspace(z_dist_range[0], z_dist_range[1], num=100, endpoint=True)
 
     margin_plot = go.Figure(
         data=[
             go.Scatter(
-                x=np.concatenate(
-                    ([dist1["range"][0] / 10], x_range, [dist1["range"][1] / 10])
-                ),
+                x=np.concatenate(([x_dist_range[0]], x_range, [x_dist_range[1]])),
                 y=np.concatenate(([0], x_dist.pdf(x_range), [0])),
                 line=dict(dash="dot"),
                 name="Knife",
             ),
             go.Scatter(
-                x=np.concatenate(
-                    ([dist2["range"][0] / 10], y_range, [dist2["range"][1] / 10])
-                ),
+                x=np.concatenate(([y_dist_range[0]], y_range, [y_dist_range[1]])),
                 y=np.concatenate(([0], y_dist.pdf(y_range), [0])),
                 line=dict(dash="longdash"),
                 name="Candle 1",
             ),
             go.Scatter(
-                x=np.concatenate(
-                    ([dist3["range"][0] / 10], z_range, [dist3["range"][1] / 10])
-                ),
+                x=np.concatenate(([z_dist_range[0]], z_range, [z_dist_range[1]])),
                 y=np.concatenate(([0], z_dist.pdf(z_range), [0])),
                 line=dict(dash="dash"),
                 name="Candle 2",
@@ -442,16 +520,32 @@ def update_graph(integration_region, dist1, dist2, dist3):
         xaxis=dict(range=[-0.2, 1.2]),
         margin=dict(l=0, r=0, t=0, b=0),
         height=400,
-        legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=1.02,
-            xanchor="right",
-            x=1
-        )
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
     )
 
-    return joint_plot, margin_plot
+    result_left, error_left = nquad(
+        lambda z, y, x: x_dist.pdf(x) * y_dist.pdf(y) * z_dist.pdf(z),
+        ranges=[
+            lambda c1, k: (max(k, z_dist_range[0]), z_dist_range[1]),  # c2 limits
+            lambda k: (y_dist_range[0], min(k, y_dist_range[1])),  # c1 limits
+            x_dist_range,  # k limits
+        ],
+        opts=dict(limit=2),
+    )
+
+    result_right, error_right = nquad(
+        lambda z, y, x: x_dist.pdf(x) * y_dist.pdf(y) * z_dist.pdf(z),
+        ranges=[
+            lambda c1, k: (z_dist_range[0], min(k, z_dist_range[1])),  # c2 limits
+            lambda k: (max(k, y_dist_range[0]), y_dist_range[1]),  # c1 limits
+            x_dist_range,  # k limits
+        ],
+        opts=dict(limit=2),
+    )
+
+    integral_estimate = result_left + result_right
+
+    return joint_plot, margin_plot, f"{integral_estimate:0.4f}", f"{mc_estimate:0.4f}"
 
 
 if __name__ == "__main__":
